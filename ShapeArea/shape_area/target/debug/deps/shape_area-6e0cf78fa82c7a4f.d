f54a77cd24ee234d29c18745ec32b68fd61bf932
je-suis-un-chat <3495894125@qq.com>
2026-03-08 16:50:31 +0800

    实现长方形、圆、三角形的面积计算函数。

---
+++ ShapeArea/shape_area/target/debug/deps/shape_area-6e0cf78fa82c7a4f.d
@@ -0,0 +1,5 @@
+/home/je-suis-un-chat/YatSenOS/colored_output/shape_area/target/debug/deps/shape_area-6e0cf78fa82c7a4f.d: src/main.rs
+
+/home/je-suis-un-chat/YatSenOS/colored_output/shape_area/target/debug/deps/libshape_area-6e0cf78fa82c7a4f.rmeta: src/main.rs
+
+src/main.rs:
