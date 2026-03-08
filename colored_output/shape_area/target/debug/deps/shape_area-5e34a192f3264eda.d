1a540ab94c16383cce8a2f3e33e052834ec2e7e1
je-suis-un-chat <3495894125@qq.com>
2026-03-08 16:53:27 +0800

    删除了放错位置的文件夹。

--- colored_output/shape_area/target/debug/deps/shape_area-5e34a192f3264eda.d
+++
@@ -1,5 +0,0 @@
-/home/je-suis-un-chat/YatSenOS/colored_output/shape_area/target/debug/deps/shape_area-5e34a192f3264eda.d: src/main.rs
-
-/home/je-suis-un-chat/YatSenOS/colored_output/shape_area/target/debug/deps/shape_area-5e34a192f3264eda: src/main.rs
-
-src/main.rs:
